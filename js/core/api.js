779f8f398ea0fc8d2f88453d8306d7b5df2e08f9
city-boy1 <kojokiani@gmail.com>
2026-06-07 19:46:40 +0000

    connect to production backend

--- js/core/api.js
+++ js/core/api.js
@@ -9,7 +9,7 @@
 
 import AppState from './state.js';
 
-const BASE_URL = window.FLOWVA_API_URL || 'http://127.0.0.1:5000/api';
+const BASE_URL = window.FLOWVA_API_URL || 'https://flowva-backend-ztai.onrender.com/api';
 
 let _refreshPromise = null;
 
